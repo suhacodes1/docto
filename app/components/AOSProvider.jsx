2fdf73517cc1f385d36fcc99601491e219cda27b
suhacodes1 <suha.fathima.au@gmail.com>
2026-03-25 15:27:07 +1100

    fixed build issue

--- app/components/AOSProvider.jsx
+++ app/components/AOSProvider.jsx
@@ -12,22 +12,5 @@ export default function AOSProvider({ children }) {
     });
   }, []);
 
-  return children;
+  return <>{children}</>;
 }
-"use client";
-
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-export default function AOSProvider({ children }) {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }, []);
-
-  return children;
-}
-
